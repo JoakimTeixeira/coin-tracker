a327349fc6b67d37ae2fe24aab9495e141bbef0d
Joakim Teixeira <setokim1@hotmail.com>
2020-11-18 15:09:24 -0300

    Add header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import CoinSummary from 'pages/CoinSummary';
 import { BrowserRouter, Route } from 'react-router-dom';
+import Header from 'components/Header';
 
 const App = () => (
   <BrowserRouter>
+    <Header />
     <Route exact path="/" component={CoinSummary} />
   </BrowserRouter>
 );
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => (
+  <header>
+    <h1 className="text-center text-warning mt-3 mb-4">Coin Tracker</h1>
+  </header>
+);
+
+export default Header;
