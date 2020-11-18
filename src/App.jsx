5c3a619415a23e795c9bd1eb88fbf0cbe2c7e3be
Joakim Teixeira <setokim1@hotmail.com>
2020-11-18 14:12:16 -0300

    Add coin summary page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import CoinSummary from 'pages/CoinSummary';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 const App = () => (
-  <div>
-    Coin Tracker
-  </div>
+  <BrowserRouter>
+    <Route exact path="/" component={CoinSummary} />
+  </BrowserRouter>
 );
 
 export default App;
---
+++ src/pages/CoinSummary.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const CoinSummary = () => (
+  <section>
+    Coin Summary
+  </section>
+);
+
+export default CoinSummary;
