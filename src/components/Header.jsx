6d443a93fff8d3d83c2b4bbacbc0958540d8da09
Joakim Teixeira <setokim1@hotmail.com>
2020-12-02 00:24:03 -0300

    Change font family

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 
 const Header = () => (
   <header>
-    <h1 className="text-center text-warning pt-5 mb-5">
+    <h1 className="title text-center text-warning pt-5 mb-5">
       <i className="fas fa-coins" />
       <span className="text-white">
-        {' Coin Tracker'}
+        <span style={{ fontWeight: 'lighter' }}>{' Coin '}</span>
+        Tracker
       </span>
     </h1>
   </header>
