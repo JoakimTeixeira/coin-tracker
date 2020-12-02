9cfb55d407348ff133a6daa0758d2ad6c3a1d0ea
Joakim Teixeira <setokim1@hotmail.com>
2020-12-02 19:55:51 -0300

    Change data order

--- src/pages/CoinDetails/CoinData.jsx
+++ src/pages/CoinDetails/CoinData.jsx
@@ -10,30 +10,32 @@ const CoinData = ({ chartData }) => {
         <Row className="d-flex flex-md-row flex-column align-items-center justify-content-center text-center">
           <Col>
             <div className="d-flex flex-column">
-              <span className="card-label text-muted">Market Cap</span>
-              <span>{formatCurrency(chartData.coin.market_cap)}</span>
-            </div>
-            <hr />
-            <div className="d-flex flex-column">
               <span className="card-label text-muted">Total Supply</span>
               <span>{formatCurrency(chartData.coin.total_supply)}</span>
             </div>
+            <hr />
+            <div className="d-flex flex-column">
+              <span className="card-label text-muted">Circulating Supply</span>
+              <span>{formatCurrency(chartData.coin.circulating_supply)}</span>
+            </div>
           </Col>
           <Col>
+            <div className="d-flex flex-column">
+              <span className="card-label text-muted">Market Cap</span>
+              <span>{formatCurrency(chartData.coin.market_cap)}</span>
+            </div>
+
+            <hr />
             <div className="d-flex flex-column">
               <span className="card-label text-muted">Volume (24h)</span>
               <span>{formatCurrency(chartData.coin.total_volume)}</span>
             </div>
-            <hr />
-            <div className="d-flex flex-column">
-              <span className="card-label text-muted">High (24h)</span>
-              <span>{formatCurrency(chartData.coin.high_24h)}</span>
-            </div>
+
           </Col>
           <Col>
             <div className="d-flex flex-column">
-              <span className="card-label text-muted">Circulating Supply</span>
-              <span>{formatCurrency(chartData.coin.circulating_supply)}</span>
+              <span className="card-label text-muted">High (24h)</span>
+              <span>{formatCurrency(chartData.coin.high_24h)}</span>
             </div>
             <hr />
             <div className="d-flex flex-column">
