6b996259dd48f84d706b32e1ba848afc639b2404
Joakim Teixeira <setokim1@hotmail.com>
2020-12-08 05:30:13 -0300

    Add search box

--- src/pages/CoinSummary/AddCoin.jsx
+++ src/pages/CoinSummary/AddCoin.jsx
@@ -1,6 +1,8 @@
 import { WatchListContext } from 'contexts/WatchListContext';
-import React, { useContext, useEffect, useState } from 'react';
-import { Dropdown } from 'react-bootstrap';
+import React, {
+  forwardRef, useContext, useEffect, useState,
+} from 'react';
+import { Dropdown, FormControl } from 'react-bootstrap';
 import coinsAPI from 'services/coinsAPI';
 import { sortArrayOfObjects, firstLetterToUppercase } from 'utils';
 
@@ -24,13 +26,43 @@ const AddCoin = () => {
     fetchData();
   }, [watchList]);
 
+  const SearchBoxMenu = forwardRef(
+    ({
+      children, style, className, 'aria-labelledby': labeledBy,
+    }, ref) => {
+      const [value, setValue] = useState('');
+
+      return (
+        <div
+          ref={ref}
+          style={style}
+          className={className}
+          aria-labelledby={labeledBy}
+        >
+          <FormControl
+            autoFocus
+            className="mx-3 my-2 w-auto"
+            placeholder="Type coin name..."
+            onChange={(e) => setValue(e.target.value)}
+            value={value}
+          />
+          <ul className="list-unstyled">
+            {React.Children.toArray(children).filter(
+              (child) => !value || child.props.children.toLowerCase().startsWith(value),
+            )}
+          </ul>
+        </div>
+      );
+    },
+  );
+
   return (
     <Dropdown>
       <Dropdown.Toggle variant="success" id="dropdown-basic">
         <i className="fas fa-plus" />
         {' ADD '}
       </Dropdown.Toggle>
-      <Dropdown.Menu>
+      <Dropdown.Menu as={SearchBoxMenu}>
         {availableCoins.map((coin) => (
           <Dropdown.Item key={coin.id} onClick={() => addCoin(coin.id)}>
             {firstLetterToUppercase(coin.name)}
